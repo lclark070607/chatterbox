4af4282362616ab057e4c68828d61d169228f3c0
Louise Clark <lclark070607@gmail.com>
2016-12-16 14:28:21 -0500

    starting work on username inbut button

--- chatter.js
+++ chatter.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
     getMessage();
-    console.log('yippee!')
+    let submitBtn = document.querySelector('#send');
+    submitBtn.addEventListener('click', addMessage);
 });
 
 function getMessage() {
@@ -22,12 +23,27 @@ function getMessage() {
 // }
 
  let parent = document.querySelector('#incoming-message');
-       for (let i = 0; response.chats.length; i++) {
+       for (let i = 0; i < response.chats.length; i++) {
            let inMessage = document.createElement('li');
+           let inUser = document.createElement('li');
            inMessage.textContent = response.chats[i].message;
+           inUser.textContent = response.chats[i].from;
            parent.appendChild(inMessage);
-           console.log(response.chats.id);
-        }   
+           parent.appendChild(inUser);
+        } 
+ 
     });
 request.send();
+}
+
+function addMessage() {
+    let push = new XMLHttpRequest();
+    push.open('POST', 'http://api.queencityiron.com/chats');
+    console.log(document.querySelector('#type'));
+    let body = JSON.stringify({ 
+        message: document.querySelector('#type').value,
+        from: document.querySelector('#myname').value,
+    });
+    document.querySelector('#type').value = '';
+    push.send(body);
 }
