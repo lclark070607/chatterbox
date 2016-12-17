cbba0a72525f2242ac78a867c413e869d828bf6a
KingPra <kingi.pra@gmail.com>
2016-12-17 09:54:57 -0500

    fixed user name position, added user name to side bar

--- chatter.js
+++ chatter.js
@@ -1,49 +1,56 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     getMessage();
     let submitBtn = document.querySelector('#send');
     submitBtn.addEventListener('click', addMessage);
+    
+
+//    getMessage();
 });
 
 function getMessage() {
     let request = new XMLHttpRequest();
     request.open('GET', 'http://api.queencityiron.com/chats');
-    request.addEventListener('load', function() {
+    request.addEventListener('load', function () {
         let response = JSON.parse(request.responseText);
- 
-//  *** wrong code ***  
-//        let parent = document.querySelector('#incoming-message');
-//        for (let i = 0; response.chats.length; i++) {
-//            let inMessage = document.createElement('li');
-//            inMessage.textContent = response.chats[i].message;
-//            parent.appendChild(inMessage);
-//            console.log(response.chats.message);
-//         }   
-//     });
-// request.send();
-// }
 
- let parent = document.querySelector('#incoming-message');
-       for (let i = 0; i < response.chats.length; i++) {
-           let inMessage = document.createElement('li');
-           let inUser = document.createElement('li');
-           inMessage.textContent = response.chats[i].message;
-           inUser.textContent = response.chats[i].from;
-           parent.appendChild(inMessage);
-           parent.appendChild(inUser);
-        } 
- 
+        let parent = document.querySelector('#incoming-message');
+        for (let i = 0; i < response.chats.length; i++) {
+            let inMessage = document.createElement('li');
+            let inUser = document.createElement('div');
+            inMessage.textContent = response.chats[i].message;
+            //  + response.chats[i].from;
+            inUser.textContent = ' username: ' + response.chats[i].from;
+            parent.appendChild(inMessage);
+            parent.appendChild(inUser);
+        }
+        let adopter = document.querySelector('#side-bar');
+        for (let i = 0; i < response.chats.length; i++) {
+            let users = document.createElement('li');
+            users.textContent = response.chats[i].from;
+            adopter.appendChild(users);
+        }
+
     });
-request.send();
+    request.send();
 }
 
 function addMessage() {
     let push = new XMLHttpRequest();
     push.open('POST', 'http://api.queencityiron.com/chats');
     console.log(document.querySelector('#type'));
-    let body = JSON.stringify({ 
+    let body = JSON.stringify({
         message: document.querySelector('#type').value,
         from: document.querySelector('#myname').value,
     });
     document.querySelector('#type').value = '';
     push.send(body);
 }
+
+// function showMyMessage() {
+//     //if (response.chats[i].from === input.value) {
+//         let parent = document.querySelector('#outgoing-message');
+//         let myMessage = document.createElement('li');
+//         myMessage.textContent = input.value;
+//         console.log(myMessage);
+//     }
+
